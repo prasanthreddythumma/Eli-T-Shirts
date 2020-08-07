4313f07264f474c8afa7aad6c032013fee7fbac5
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-08-06 21:43:30 -0400

    added Formal shirt fragment code

--- app/src/main/java/com/example/eli_tshirts/FormalShirtFragment.java
+++ app/src/main/java/com/example/eli_tshirts/FormalShirtFragment.java
@@ -1,58 +1,27 @@
 package com.example.eli_tshirts;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FormalShirtFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FormalShirtFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class FormalShirtFragment extends Fragment implements View.OnClickListener{
+    CardView cardView1,cardView2,cardView3,cardView4,cardView5, cardView6;
     public FormalShirtFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FormalShirtFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FormalShirtFragment newInstance(String param1, String param2) {
-        FormalShirtFragment fragment = new FormalShirtFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -60,5 +29,31 @@ public class FormalShirtFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_formal_shirt, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cardView1 = view.findViewById(R.id.cardview);
+        cardView2 = view.findViewById(R.id.cardview2);
+        cardView3 = view.findViewById(R.id.cardview3);
+        cardView4 = view.findViewById(R.id.cardview1);
+        cardView5 = view.findViewById(R.id.cardview4);
+        cardView6 = view.findViewById(R.id.cardview5);
+
+        cardView1.setOnClickListener(this);
+        cardView2.setOnClickListener(this);
+        cardView3.setOnClickListener(this);
+        cardView4.setOnClickListener(this);
+        cardView5.setOnClickListener(this);
+        cardView6.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if(id == R.id.cardview ||id == R.id.cardview1||id == R.id.cardview2||id == R.id.cardview3||id == R.id.cardview4||id == R.id.cardview5){
+            startActivity(new Intent(getActivity(), ItemDetailsActivity.class));
+        }
     }
 }
